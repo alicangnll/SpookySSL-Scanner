561c0a3a8ae92f5d096362e9b17dbf2ac2840b7d
Ali Can Gönüllü <alicangonullu@yahoo.com>
2022-11-02 12:29:28 +0300

    SpookySSL Scanner
    SpookySSL Scanner

---
+++ spooky.py
@@ -0,0 +1,46 @@
+import platform, glob
+from win32api import *
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def get_version_number(file_path):
+    File_information = GetFileVersionInfo(file_path, "\\")
+    ms_file_version = File_information['FileVersionMS']
+    ls_file_version = File_information['FileVersionLS']
+    return [str(HIWORD(ms_file_version)), str(LOWORD(ms_file_version)), str(HIWORD(ls_file_version)), str(LOWORD(ls_file_version))]
+
+def scanlocal(disk):
+    if(platform.system() == "Windows"):
+        print("Finding DLL files...")
+        dlllist = glob.glob('' + str(disk) + ':\\**\libssl*.dll', recursive=True)
+        for file_path in dlllist:
+            version = ".".join(get_version_number(file_path))
+            if("1.1.1.0" < version < "1.1.1.99"):
+                print(bcolors.OKGREEN + file_path + " (" + version + ") IS NOT VULNERABLE!" + bcolors.ENDC)
+            elif("3.0.0" < version < "3.0.6"):
+                print(bcolors.WARNING + file_path + " (" + version + ") IS VULNERABLE!" + bcolors.ENDC)
+    else:
+        import osquery
+        instance = osquery.SpawnInstance()
+        instance.open()
+        print("Scanning for Debian-based...")
+        print(instance.client.query('select name, version from deb_packages where name like "openssl" and version like "3.0%"; > spookyssl-logs.log'))
+        print("Scanning for RHEL-based...")
+        print(instance.client.query('select name, version from rpm_packages where name like "openssl" and version like "3.0%"; > spookyssl-logs.log'))
+        print("Scanning for macOS...")
+        print(instance.client.query('SELECT * FROM homebrew_packages WHERE name LIKE "openssl" and version like "3.0%"; > spookyssl-logs.log'))
+
+print("====================================================")
+print(" SpookySSL CVE-2022-3602 SSLv3 Scanner (11-02-2022) ")
+print("====================================================")
+disk = input("Write your disk name (ex. C, D) : ")
+scanlocal(disk)
